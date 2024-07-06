5b45cb0c46f1e2149998dba273e33727214f4c97
Poluri Priyanka <priyankapoluri325@gmail.com>
2024-07-06 23:18:47 +0530

    implemented get related events

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,5 +1,7 @@
+
 import CheckoutButton from '@/components/shared/CheckoutButton';
-import { getEventById } from '@/lib/actions/event.actions'
+import Collection from '@/components/shared/Collection';
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
 import Image from 'next/image';
@@ -7,7 +9,14 @@ import Image from 'next/image';
 const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
   const event = await getEventById(id);
 
+  const relatedEvents = await getRelatedEventsByCategory({
+    categoryId: event.category._id,
+    eventId: event._id,
+    page: searchParams.page as string,
+  })
+
   return (
+    <>
     <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
       <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
         <Image 
@@ -70,6 +79,22 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
         </div>
       </div>
     </section>
+
+    {/* EVENTS with the same category */}
+    <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      <h2 className="h2-bold">Related Events</h2>
+
+      <Collection 
+          data={relatedEvents?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={3}
+          page={searchParams.page as string}
+          totalPages={relatedEvents?.totalPages}
+        />
+    </section>
+    </>
   )
 }
 
