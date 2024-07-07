f630d0b538e138bf6603eb277be1333d6cc0bb80
Poluri Priyanka <priyankapoluri325@gmail.com>
2024-07-07 22:23:24 +0530

    pushing componeent,config and other files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
-import CategoryFilter from '@/components/shared/CategoryFilter';
-import Search from '@/components/shared/Search';
+// import Collection from '@/components/shared/Collection'
+// import CategoryFilter from '@/components/shared/CategoryFilter';
+// import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
+//import { getAllEvents } from '@/lib/actions/event.actions';
 import Image from 'next/image'
 import Link from 'next/link'
+import { SearchParamProps } from '@/types';
 
-export default function Home() {
- 
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  // const events = await getAllEvents({
+  //   query: '',
+  //   category: '',
+  //   page: 1,
+  //   limit: 6
+  // })
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -34,13 +47,20 @@ export default function Home() {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          <Search />
-          <CategoryFilter />
+        {/* <Search />
+        <CategoryFilter /> */}
         </div>
-        </section>
-        </>
+
+        {/* <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        /> */}
+      </section>
+    </>
   )
 }
-    
-
-       
