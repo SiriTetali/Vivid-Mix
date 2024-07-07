31703e01f8dc76e11045344c9594d6187c41c75a
Poluri Priyanka <priyankapoluri325@gmail.com>
2024-07-07 19:16:01 +0530

    added orders page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -34,6 +34,19 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
+      <section className="wrapper my-8">
+        <Collection 
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
+
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
